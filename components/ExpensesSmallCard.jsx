73de3e82d6d3955d8362e99423a514db3259a4e6
Tomas Jara <tomasignaciojd@gmail.com>
2024-11-12 23:06:37 -0300

    Fix: anchura de descripcion en tarjeta de gastos

--- components/ExpensesSmallCard.jsx
+++ components/ExpensesSmallCard.jsx
@@ -1,9 +1,7 @@
-import { useEffect, useState } from 'react'
 import dayjs from "dayjs"
 import { Pressable, Text, View } from "react-native"
 import { formatMoney } from '@/utils/formatMoney'
 import { dayOfWeek } from '@/utils/dayOfWeek'
-import { useExpensesStore } from '@/store/expensesStore'
 
 const CategoryComponent = ({ name = '', color = 'blue' }) => (
     <Text style={{ backgroundColor: color, width: 'auto', color: 'white', fontWeight: '600', padding: 4, borderRadius: 7, fontSize: 10 }}>{name}</Text>
@@ -14,26 +12,24 @@ export const ExpensesSmallCard = ({ expense, onPress = () => { } }) => {
 
     const dayWeek = dayOfWeek('small', dayjs(paymentDate).day() - 1)
     const day = dayjs(paymentDate).date().toString().padStart(2, '0')
-    const month = (dayjs(paymentDate).month() + 1).toString().padStart(2, '0')
-    const year = dayjs(paymentDate).year()
     const moneyFormatted = formatMoney(value)
 
     return (
         <Pressable style={{ flexDirection: 'row', justifyContent: 'space-between', borderWidth: 1, marginBottom: 10, borderColor: '#0000007a', paddingVertical: 9, paddingHorizontal: 12, borderRadius: 10 }} onPress={onPress}>
-            <View style={{ gap: 5, maxWidth: 220 }}>
+            <View style={{ gap: 5, maxWidth: 200 }}>
                 <View style={{ flexDirection: 'row', gap: 5, alignItems: 'center' }}>
-                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <View style={{ flexDirection: 'row', alignItems: 'center', gap: 3 }}>
                         <Text>{day}</Text>
                         <Text style={{ fontSize: 11, opacity: 0.5 }}>{dayWeek}</Text>
                     </View>
                     <Text style={{ fontSize: 11, opacity: 0.5 }}>{dayjs(paymentDate).format('DD/MM/YYYY')}</Text>
                 </View>
                 <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                    <View style={{ flexDirection: 'row', justifyContent: 'center', gap: 5, flexWrap: 'wrap', maxWidth: 240 }}>
+                    <View style={{ flexDirection: 'row', justifyContent: 'center', gap: 5, flexWrap: 'wrap', maxWidth: 230 }}>
                         {/* <CategoryComponent name={'Texto largoooooooooooo000000000000000000000000000000000largoooooooooooo000000000000000000000000000000000'} /> */}
                         {/* <CategoryComponent name={'Texto largoooooooooooo000000000000000000000000000000000largoooooooooooo000000000000000000000000000000000'} color='green' /> */}
-                        <CategoryComponent name={category?.name ? category.name : ''} />
-                        <CategoryComponent name={paymentMethod?.name ? paymentMethod.name : ''} color='green' />
+                        <CategoryComponent name={category?.name ? category.name : ''} color={category?.color ? category.color : 'blue'} />
+                        <CategoryComponent name={paymentMethod?.name ? paymentMethod.name : ''} color={paymentMethod?.color ? paymentMethod.color : 'green'} />
                     </View>
                 </View>
                 {description && <View style={{ flexDirection: 'row' }}>
